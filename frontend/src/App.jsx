362d0b4ffa6891376100ee5c9fb7c45aa46348af
Braynkanth <braynka-im22062@stu.kln.ac.lk>
2025-07-31 22:10:21 +0530

    routes added in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,41 @@
+import { Route, Routes } from "react-router-dom";
+import StuDashboardpage from './pages/StuDashboardpage';
+import TeachDashboardpage from './pages/TeachDashboardpage';
+import ProtectedRoute from './components/ProtectedRoute';
+import ForgotPasswordPage  from "./pages/ForgotPasswordPage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage"; 
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
+function App() {
 
-export default function App() {
   return (
-    <>
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
-    </>
+   
+  
+    <Routes>
+        <Route path='/' element={<SignUpPage />} />
+        <Route path='/forgot-password' element={<ForgotPasswordPage/>} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/verify-email' element={<EmailVerificationPage />} />
+       <Route path="/student-dashboard" element={<ProtectedRoute allowedRoles={['student']}><StuDashboardpage /></ProtectedRoute> }
+/>
+
+<Route
+  path="/teacher-dashboard"
+  element={
+    <ProtectedRoute allowedRoles={['teacher']}>
+      <TeachDashboardpage />
+    </ProtectedRoute>
+  }
+/>
+
+        
+      </Routes>
+
   )
 }
 
-
+export default App;
