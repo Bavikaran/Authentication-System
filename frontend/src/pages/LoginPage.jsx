8bf71b3bba4d5e8969b590e5a7ba038ae7329da3
Braynkanth <braynka-im22062@stu.kln.ac.lk>
2025-08-03 03:44:07 +0530

    logo changed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { Mail, Lock, Loader } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import Input from '../components/Input';
-import { useAuthStore } from '../store/authStore'; // Assuming you are using Zustand
+import useAuthStore from './store/authStore'; // Assuming you are using Zustand
 // eslint-disable-next-line no-unused-vars
 import axios from 'axios';
 
